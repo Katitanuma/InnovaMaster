37819b1c197af568f74078c30243bc36941f8051
Johan Chevez <johanchevez59@hotmail.com>
2017-03-18 16:45:07 -0600

    Codificación de Login y Presentación

--- INNOVAMASTER/Formularios/FrmLogin.vb
+++ INNOVAMASTER/Formularios/FrmLogin.vb
@@ -1,3 +1,114 @@
-﻿Public Class FrmLogin
+﻿Imports System.ComponentModel
 
+Public Class FrmLogin
+
+    Dim Conec As New Conexion
+
+
+    Private Function Validar() As Boolean
+        Dim estado As Boolean
+        If TxtUsuario.Text = Nothing And TxtContrasena.Text = Nothing Then
+            TxtUsuario.Focus()
+            EpMensaje.SetError(TxtUsuario, "Ingrese el Usuario")
+            EpMensaje.SetError(TxtContrasena, "Ingrese la Contraseña")
+            estado = False
+        ElseIf TxtUsuario.Text = Nothing Then
+            TxtUsuario.Focus()
+            EpMensaje.SetError(TxtUsuario, "Ingrese el Usuario")
+            EpMensaje.SetError(TxtContrasena, "")
+            estado = False
+        ElseIf TxtContrasena.Text = Nothing Then
+            TxtContrasena.Focus()
+            EpMensaje.SetError(TxtUsuario, "")
+            EpMensaje.SetError(TxtContrasena, "Ingrese la Contraseña")
+            estado = False
+        Else
+            EpMensaje.SetError(TxtUsuario, "")
+            EpMensaje.SetError(TxtContrasena, "")
+            estado = True
+        End If
+        Return estado
+    End Function
+    Private Sub FrmLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Focus()
+    End Sub
+
+
+    Private Sub BtnCerrar_Click(sender As Object, e As EventArgs) Handles BtnCerrar.Click
+        Application.Exit()
+        My.Settings.Save()
+    End Sub
+
+    Private Sub BtnIniciar_Click(sender As Object, e As EventArgs) Handles BtnIniciar.Click
+        If Validar() Then
+            Try
+                Conec.Conectarse()
+                Dim dr As SqlClient.SqlDataReader
+                Dim cmd As New SqlClient.SqlCommand("Logeo", Conec.Con)
+                cmd.CommandType = CommandType.StoredProcedure
+                cmd.Parameters.AddWithValue("@Usuario", Replace(TxtUsuario.Text, "'", ""))
+                cmd.Parameters.AddWithValue("@Contrasena", Replace(SHA1(TxtContrasena.Text), "'", ""))
+
+
+                dr = cmd.ExecuteReader
+                If dr.Read Then
+                    MenuPrincipal.LblIdUsuario.Text = dr.GetValue(0).ToString
+                    If dr.GetValue(1).ToString = "Administrador" Then
+                        MenuPrincipal.PagEmpleados.Enabled = True
+                        MenuPrincipal.PagUsuarios.Enabled = True
+                        MenuPrincipal.PagProductos.Enabled = True
+                        MenuPrincipal.PagUbicacion.Enabled = True
+                        MenuPrincipal.PagConfiguracion.Enabled = True
+                    Else
+                        MenuPrincipal.PagEmpleados.Enabled = False
+                        MenuPrincipal.PagUsuarios.Enabled = False
+                        MenuPrincipal.PagProductos.Enabled = False
+                        MenuPrincipal.PagUbicacion.Enabled = False
+                        MenuPrincipal.PagConfiguracion.Enabled = False
+                        frmCargo.Close()
+                        FrmBackup.Close()
+                        FrmConfiguracion.Close()
+                        FrmMunicipio.Close()
+                        FrmDepartamento.Close()
+                        FrmPais.Close()
+                        FrmProducto.Close()
+                        FrmCategoria.Close()
+                        FrmMarca.Close()
+                        FrmModelo.Close()
+                        FrmUsuario.Close()
+                        FrmEmpleado.Close()
+                        FrmProfesiones.Close()
+                    End If
+
+                    Me.Hide()
+                    MenuPrincipal.Show()
+                Else
+                    MsgBox("Usuario o Contraseña No Valido", MsgBoxStyle.Critical)
+                End If
+
+            Catch ex As Exception
+                MsgBox(ex.ToString)
+            End Try
+        End If
+    End Sub
+
+    Private Sub BtnConfiguracion_Click(sender As Object, e As EventArgs) Handles BtnConfiguracion.Click
+        FrmConfiguracion.Show()
+    End Sub
+
+    Private Sub ChkVer_CheckedChanged_1(sender As Object, e As EventArgs) Handles ChkVer.CheckedChanged
+        If ChkVer.Checked = True Then
+            TxtContrasena.UseSystemPasswordChar = False
+        Else
+            TxtContrasena.UseSystemPasswordChar = True
+        End If
+    End Sub
+
+
+
+    Private Sub TxtContrasena_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TxtContrasena.KeyPress
+        If Asc(e.KeyChar) = 13 Then
+            BtnIniciar.PerformClick()
+        End If
+    End Sub
 End Class
--- INNOVAMASTER/Formularios/FrmPresentacion.vb
+++ INNOVAMASTER/Formularios/FrmPresentacion.vb
@@ -1,3 +1,66 @@
 ﻿Public Class FrmPresentacion
+    Dim i As Integer
+    Private Sub FrmPresentacion_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Interval = 200
+        Timer1.Start()
+        i = 0
+    End Sub
 
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        i += 1
+        If i = 1 Then
+            LblProgreso.Text = "Estableciendo Procesos..."
+        ElseIf i = 2 Then
+            LblProgreso.Text = "Estableciendo Procesos......."
+        ElseIf i = 3 Then
+            LblProgreso.Text = "Estableciendo Procesos..."
+        ElseIf i = 4 Then
+            LblProgreso.Text = "Inicializando Procesos..."
+        ElseIf i = 5 Then
+            LblProgreso.Text = "Inicializando Procesos......."
+        ElseIf i = 6 Then
+            LblProgreso.Text = "Inicializando Procesos..."
+        ElseIf i = 7 Then
+            LblProgreso.Text = "Estableciendo Estructuras..."
+        ElseIf i = 8 Then
+            LblProgreso.Text = "Estableciendo Estructuras......"
+        ElseIf i = 9 Then
+            LblProgreso.Text = "Estableciendo Estructuras..."
+        ElseIf i = 10 Then
+            LblProgreso.Text = "Implementanto Estructuras..."
+        ElseIf i = 11 Then
+            LblProgreso.Text = "Implementanto Estructuras......."
+        ElseIf i = 12 Then
+            LblProgreso.Text = "Implementanto Estructuras..."
+        ElseIf i = 13 Then
+            LblProgreso.Text = "Recopilando Datos..."
+        ElseIf i = 14 Then
+            LblProgreso.Text = "Recopilando Datos......."
+        ElseIf i = 15 Then
+            LblProgreso.Text = "Recopilando Datos..."
+        ElseIf i = 16 Then
+            LblProgreso.Text = "Finalizando Procesos..."
+        ElseIf i = 17 Then
+            LblProgreso.Text = "Finalizando Procesos......"
+        ElseIf i = 18 Then
+            LblProgreso.Text = "Finalizando Procesos..."
+        ElseIf i = 19 Then
+            LblProgreso.Text = "Listo..."
+        ElseIf i = 20 Then
+            LblProgreso.Text = "Listo..."
+        ElseIf i = 21 Then
+            LblProgreso.Text = "DISFRUTA DE NUESTROS SERVICIOS"
+        ElseIf i = 22 Then
+            LblProgreso.Text = "DISFRUTA DE NUESTROS SERVICIOS"
+        ElseIf i = 23 Then
+            LblProgreso.Text = "DISFRUTA DE NUESTROS SERVICIOS"
+        ElseIf i = 24 Then
+            LblProgreso.Text = "DISFRUTA DE NUESTROS SERVICIOS"
+        ElseIf i = 25 Then
+            LblProgreso.Text = "DISFRUTA DE NUESTROS SERVICIOS"
+            Timer1.Stop()
+            Me.Hide()
+            FrmLogin.Show()
+        End If
+    End Sub
 End Class
