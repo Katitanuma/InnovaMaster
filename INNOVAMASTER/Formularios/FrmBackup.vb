9f60d2768823d59e067634e07eb7a4897a5efd17
Carmen Gudiel <carmengudiel625@gmail.com>
2017-03-18 18:25:40 -0500

    Codificación de Configuración y BackUp

--- INNOVAMASTER/Formularios/FrmBackup.vb
+++ INNOVAMASTER/Formularios/FrmBackup.vb
@@ -1,3 +1,66 @@
-﻿Public Class FrmBackup
+﻿Imports System.Data.SqlClient
+Public Class FrmBackup
+    Dim cmd As SqlCommand
+    Dim conec As New Conexion
+    Private Sub FrmBackup_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            conec.Conectarse()
+            cmd = New SqlCommand("sp_spaceused", conec.Con)
+            cmd.CommandType = CommandType.StoredProcedure
 
+            Dim Dt As New DataTable
+            Dim Da As SqlDataAdapter = New SqlDataAdapter(cmd)
+
+            Da.Fill(Dt)
+
+            Dim Dr As DataRow = Dt.Rows.Item(0)
+
+            lblDBName.Text = Dr.Item("database_name").ToString.Trim
+            lblDBSize.Text = Dr.Item("database_size").ToString.Trim
+            lblUnallocatedSize.Text = Dr.Item("unallocated space").ToString.Trim
+
+            btnSelectDir.Enabled = True
+            txtBackupName.Enabled = True
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+    Private Sub btnBackup_Click(sender As Object, e As EventArgs) Handles btnBackup.Click
+        btnBackup.Text = "Respaldando..."
+        btnBackup.Enabled = False
+        btnBackup.Refresh()
+        Dim Query As String = "BACKUP DATABASE " & "INNOVAMASTERBD".Trim & "
+                                TO DISK = '" & txtPath.Text & "\" & txtBackupName.Text & ".bak'
+                                   WITH FORMAT,
+                                      MEDIANAME = '" & txtBackupName.Text & "',
+                                      NAME = '" & txtBackupName.Text & "'"
+        Try
+            conec.Conectarse()
+            cmd = New SqlCommand(Query, conec.Con)
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Se ha respaldado la base de datos correctamente.", "Respaldo",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+        Finally
+
+            btnBackup.Text = "Respaldar"
+            btnBackup.Enabled = True
+        End Try
+    End Sub
+    Private Sub btnSelectDir_Click(sender As Object, e As EventArgs) Handles btnSelectDir.Click
+        Dim FBD As New FolderBrowserDialog
+        If FBD.ShowDialog() = DialogResult.OK Then
+            txtPath.Text = FBD.SelectedPath
+        End If
+    End Sub
+    Private Sub txtBackupName_TextChanged(sender As Object, e As EventArgs) Handles txtBackupName.TextChanged
+        If Not String.IsNullOrWhiteSpace(txtBackupName.Text) Then
+            btnBackup.Enabled = True
+        Else
+            btnBackup.Enabled = False
+        End If
+    End Sub
 End Class
--- INNOVAMASTER/Formularios/FrmConfiguracion.vb
+++ INNOVAMASTER/Formularios/FrmConfiguracion.vb
@@ -1,3 +1,68 @@
-﻿Public Class FrmConfiguracion
+﻿Imports System.ComponentModel
+Imports System.Data.SqlClient
+Public Class FrmConfiguracion
+    Dim conec As New Conexion
+    Dim EC As Estado_Conexion
+    Dim CSBuilder As New SqlConnectionStringBuilder
+    Private Enum Estado_Conexion
+        NoComprobada
+        Establecida
+        NoEstablecida
+    End Enum
+    Private Sub txts_TextChanged(sender As Object, e As EventArgs) Handles _
+        txtServer.TextChanged, txtDB.TextChanged, txtUsername.TextChanged, txtPassword.TextChanged
+        EC = Estado_Conexion.NoComprobada
+        lblConnStatus.Text = "Conexión no comprobada."
+        If Not String.IsNullOrWhiteSpace(txtServer.Text) And Not String.IsNullOrWhiteSpace(txtDB.Text) And
+                Not String.IsNullOrWhiteSpace(txtUsername.Text) And Not String.IsNullOrWhiteSpace(txtPassword.Text) Then
+            btnTestConnection.Enabled = True
+        Else
+            btnTestConnection.Enabled = False
+        End If
+    End Sub
+    Private Sub btnTestConnection_Click(sender As Object, e As EventArgs) Handles btnTestConnection.Click
+        btnTestConnection.Enabled = False
+        lblConnStatus.Text = "Comprobando la conexión..."
+        lblConnStatus.Refresh()
+        With CSBuilder
+            .DataSource = txtServer.Text : .InitialCatalog = txtDB.Text : .IntegratedSecurity = False
+            .UserID = txtUsername.Text : .Password = txtPassword.Text
+        End With
+        Dim Cnx As New SqlConnection(CSBuilder.ConnectionString)
+        Try
+            Cnx.Open()
+            EC = Estado_Conexion.Establecida
+            lblConnStatus.Text = "Conexión establecida exitosamente."
+            Dim nuevaCadenaConexion As String = "Data Source=" & txtServer.Text & ";Initial Catalog=" & txtDB.Text & ";Persist Security Info=True;User ID=" & txtUsername.Text & ";Password=" & txtPassword.Text & ""
+            My.Settings.CadenaConexion = nuevaCadenaConexion
+
+            My.Settings.Servidor = txtServer.Text
+            My.Settings.BaseDatos = txtDB.Text
+            My.Settings.Usuario = txtUsername.Text
+            My.Settings.Contraseña = txtPassword.Text
+            My.Settings.Save()
+            My.Settings.Reload()
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString, "Error en la conexión", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            EC = Estado_Conexion.NoEstablecida
+            lblConnStatus.Text = "Error en la conexión"
+        Finally
+            btnTestConnection.Enabled = True
+            Cnx.Close()
+        End Try
+
+    End Sub
+    Private Sub FrmConfiguracion_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        My.Settings.Reload()
+        txtServer.Text = My.Settings.Servidor
+        txtDB.Text = My.Settings.BaseDatos
+        txtUsername.Text = My.Settings.Usuario
+        txtPassword.Text = My.Settings.Contraseña
+    End Sub
+    Private Sub FrmConfiguracion_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        My.Settings.Save()
+    End Sub
+
+
 
 End Class
